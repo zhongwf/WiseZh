cc7fb11f4fa1897403e24d37dc0ad29cfa61e2cb
zhongwf <wenfeng.zhong@gmail.com>
2016-04-16 16:48:42 +0800

    Added files via upload

---
+++ assets/source/template.js
@@ -0,0 +1,16 @@
+ //Enabling drop down navigation for mobile screen sizes
+
+ 
+
+//Adjusting the sub-menu positions so they are centered
+document.onreadystatechange = function() {
+    if (document.readyState === 'complete') {
+	 	$("ul.ww-nav ul.dropdown-menu").each(function(){
+	 		parentWidth = $(this).parent().innerWidth();
+	 		menuWidth = $(this).innerWidth();
+	 		margin = (parentWidth / 2 ) - (menuWidth / 2);
+	 		margin = margin + "px";
+	 		$(this).css("margin-left", margin);
+	 	});
+ 	}
+}
